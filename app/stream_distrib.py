d127f73c266e84c24379cf3bf98ffca34800b36b
Guido Weksler <39995669+Guidowe@users.noreply.github.com>
2025-05-27 16:52:55 -0300

    doble distrib

--- app/stream_distrib.py
+++ app/stream_distrib.py
@@ -16,11 +16,12 @@ def show_page_distrib():
     with col1:
         categoria = st.radio("Elegí una categoria", unique_categorias)
     with col2:
-        categorias2 = st.checkbox("Graficar con dos categorias", unique_categorias2,index=None)
+        categorias2 = st.radio("Graficar con dos categorias", unique_categorias2, value=None)
     if categorias2 == None:
         df_filtrado = dframe[dframe.variable_interes == categoria]
     else: 
-        df_filtrado = dframe2[dframe2.variable_interes == categorias2]
+        combined = f"{categoria}-{categorias2}"
+        df_filtrado = dframe2[dframe2.variable_interes == combined]
     st.write(f"Distribucion del empleo según la variable: {categoria}")
     chart_data = pd.DataFrame(
         {
