15df0ea8966c1d0fec10fc75ee243d9f7b3aec10
Guido Weksler <39995669+Guidowe@users.noreply.github.com>
2025-05-27 16:56:40 -0300

    va

--- app/stream_distrib.py
+++ app/stream_distrib.py
@@ -10,18 +10,18 @@ def fetch_data_distrib():
 def show_page_distrib():
     dframe,dframe2 = fetch_data_distrib()
     unique_categorias = list(set(dframe.variable_interes))
-    unique_categorias2 = list(set(dframe2.variable_interes))   
+    #unique_categorias2 = list(set(dframe2.variable_interes))   
     st.title(f"Prueba para aplicacion de precariedad mundial")
     col1, col2 = st.columns(2)
     with col1:
         categoria = st.radio("Elegí una categoria", unique_categorias)
     with col2:
-        categorias2 = st.radio("Graficar con dos categorias", unique_categorias2, value=None)
-        if categorias2 == None:
-            df_filtrado = dframe[dframe.variable_interes == categoria]
-        else: 
-            combined = f"{categoria}-{categorias2}"
-            df_filtrado = dframe2[dframe2.variable_interes == combined]
+        categorias2 = st.radio("Graficar con dos categorias", unique_categorias, value=None)
+    if categorias2 == None:
+        df_filtrado = dframe[dframe.variable_interes == categoria]
+    else: 
+        combined = f"{categoria}-{categorias2}"
+        df_filtrado = dframe2[dframe2.variable_interes == combined]
     st.write(f"Distribucion del empleo según la variable: {categoria}")
     chart_data = pd.DataFrame(
         {
