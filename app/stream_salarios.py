42824806be2ba00421ade6b56df181a53da93058
Guido Weksler <39995669+Guidowe@users.noreply.github.com>
2025-06-11 12:19:36 -0300

    agrego panel salarios

--- app/stream_salarios.py
+++ app/stream_salarios.py
@@ -1,5 +1,70 @@
 import streamlit as st
 import pandas as pd
 
+def fetch_data_salarios():
+    df = pd.read_csv('app/data/salarios_categoria.csv')
+    df['categoria'] = pd.Categorical(df['categoria'], categories=pd.unique(df['categoria']), ordered=True)
+    df2 = pd.read_csv('app/data/salarios_categoria2.csv')
+    df2['categoria'] = pd.Categorical(df2['categoria'], categories=pd.unique(df2['categoria']), ordered=True)
+    
+    return df, df2
+
 def show_page_salarios():
-    "1"
+    dframe, dframe2 = fetch_data_salarios()
+    unique_categorias = list(set(dframe.variable_interes))
+    unique_paises = sorted(list(set(dframe.PAIS)))
+    variables_salario_dict = {
+        'sal_prom': 'Salario promedio (ponderado)',
+        'sal_median': 'Mediana del salario (muestral)', 
+    }
+
+    st.title("📊 Precariedad Laboral Mundial")
+    # Sidebar for filters
+    with st.sidebar:
+        st.header("Filtros")
+        categoria = st.radio("🎯 Elegí una categoría", unique_categorias)
+        eleccion = st.radio("Desagregar por una segunda variable?", ("No", "Si"))
+        if eleccion == "Si":
+            categorias2 = st.radio("Elegir segunda categoria", unique_categorias, index=1)
+        salario_key = st.radio("📈 Cambiar la estimación del salario", 
+                            list(variables_salario_dict.keys()),
+                            format_func=lambda x: variables_salario_dict[x])
+        
+        # Button to select all countries
+        if st.button("Seleccionar todos los países"):
+            st.session_state.paises_seleccionados_salario = unique_paises
+        
+        paises_seleccionados = st.multiselect(
+            "Seleccionar países", 
+            unique_paises, 
+            default=st.session_state.get('paises_seleccionados_salario', unique_paises)
+        )
+        
+        # Update session state
+        st.session_state.paises_seleccionados_salario = paises_seleccionados
+    
+    if eleccion == "No":
+        df_filtrado = dframe[(dframe.variable_interes == categoria) & (dframe.PAIS.isin(paises_seleccionados))]
+        st.write(f"Salario promedio según: {categoria}")
+    else:
+        combined = f"{categoria}-{categorias2}"
+        combined_reverse = f"{categorias2}-{categoria}"
+        df_filtrado = dframe2[(dframe2.variable_interes.isin([combined, combined_reverse])) & (dframe2.PAIS.isin(paises_seleccionados))]
+        st.write(f"alario promedio según: {categoria} y {categorias2}")
+
+    st.markdown(f"*Variable analizada: {variables_salario_dict[salario_key]}. Estimado en dolares de paridad de poder adquisitivo de 2017*")
+    
+    chart_data = pd.DataFrame(
+        {
+        "pais": df_filtrado["PAIS"],
+        "salario": df_filtrado[salario_key],
+        "categoria": df_filtrado["categoria"],
+        }
+        )
+    
+    st.bar_chart(chart_data, x="pais", y="salario", color="categoria",stack=False)
+
+
+if __name__ == "__main__":
+    while True:
+        show_page_salarios()
--- scripts/para_app.R
+++ scripts/para_app.R
@@ -1,10 +1,20 @@
+#Importacion ####
 library(tidyverse)
 base <- readRDS("base_homogenea.RDS")
 options(scipen = 999)
 
-table(base$CATOCUP,base$TAMA)
-table(base$CATOCUP,base$CALIF)
+#Chequeos ####
+#table(base$CATOCUP,base$TAMA)
+#table(base$CATOCUP,base$CALIF)
+
+#Definiciones ####
 vector_categoricas <- c("SEXO","SECTOR","EDUC","TAMA","CALIF") 
+
+niveles <- c("Mujer","Varon","Priv","Pub","SD","Resto",
+             "Primaria","Secundaria","Terciaria",
+             "Pequeño","Mediano","Grande",
+             "Baja","Media","Alta")
+
 #Distrib del empleo ####
 proporciones_empleo <- data.frame()
 for (variable in vector_categoricas) {
@@ -20,10 +30,7 @@ for (variable in vector_categoricas) {
   
   proporciones_empleo <- bind_rows(proporciones_empleo,peso_categoria) 
 }
-niveles <- c("Mujer","Varon","Priv","Pub","SD","Resto",
-             "Primaria","Secundaria","Terciaria",
-             "Pequeño","Mediano","Grande",
-             "Baja","Media","Alta")
+
 
 proporciones_empleo <- proporciones_empleo %>% 
   mutate(categoria = factor(categoria,
@@ -34,6 +41,7 @@ write_csv(proporciones_empleo,"app/data/pesos_categoria.csv")
 
 combinations <- combn(vector_categoricas, 2, simplify = FALSE)
 proporciones_empleo_2 <- data.frame()   
+##Doble categoria ####
 
 for (combo in combinations) {
   # Combo contains the pair of variables to group by
@@ -74,8 +82,6 @@ precariedad <- base %>%
     tasa_reg = sum(WEIGHT[PRECAREG==1],na.rm = T)/sum(WEIGHT[PRECAREG %in% 0:1],na.rm = T),
     tasa_temp = sum(WEIGHT[PRECATEMP==1],na.rm = T)/sum(WEIGHT[PRECATEMP %in% 0:1],na.rm = T)) 
 
-##Por categorias ####
-
 precariedad_categorias <- data.frame()
 for (variable in vector_categoricas) {
   
@@ -98,3 +104,97 @@ precariedad_categorias <- bind_rows(precariedad_categorias,precariedad)
 write_csv(precariedad_categorias,"app/data/precariedad_categoria.csv")
 
 #Salarios ####
+salarios_arg <- base %>% 
+  filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS == "Argentina") %>% 
+  group_by(PAIS,ANO,PERIODO) %>% 
+  summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT_W,na.rm = T),
+            sal_median = median(ING_PPA)) 
+salarios_resto <- base %>% 
+  filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS != "Argentina") %>% 
+  group_by(PAIS,ANO,PERIODO) %>% 
+  summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT,na.rm = T),
+            sal_median = median(ING_PPA)) 
+
+salarios <- bind_rows(salarios_arg,salarios_resto) %>% 
+  group_by(PAIS,ANO) %>% 
+  summarise(sal_prom = mean(sal_prom,na.rm = T),
+            sal_median = mean(sal_median,na.rm = T),
+            variable_interes = "Total",
+            categoria = "Total")
+salarios_categorias <- data.frame()
+for (variable in vector_categoricas) {
+  
+  
+  salarios_arg_cat <- base %>% 
+    filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS == "Argentina") %>% 
+    filter(!is.na(.data[[variable]])) %>% 
+    group_by(PAIS,ANO,PERIODO,categoria = .data[[variable]]) %>% 
+    summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT_W,na.rm = T),
+              sal_median = median(ING_PPA)) 
+  
+  salarios_resto_cat <- base %>% 
+    filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS != "Argentina") %>% 
+    filter(!is.na(.data[[variable]])) %>% 
+    group_by(PAIS,ANO,PERIODO,categoria = .data[[variable]]) %>% 
+    summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT,na.rm = T),
+              sal_median = median(ING_PPA)) 
+  
+  salarios_cat <- bind_rows(salarios_arg_cat,salarios_resto_cat) %>% 
+    group_by(PAIS,ANO,categoria) %>% 
+    summarise(sal_prom = mean(sal_prom,na.rm = T),
+              sal_median = mean(sal_median,na.rm = T)) %>% 
+    mutate(variable_interes = variable) 
+  
+  
+  salarios_categorias <- bind_rows(salarios_cat,salarios_categorias) 
+}
+salarios_categs <- bind_rows(salarios_categorias,salarios)
+write_csv(salarios_categs,"app/data/salarios_categoria.csv")
+  
+
+salarios_categorias2 <- data.frame()   
+## Doble categoria ####
+for (combo in combinations) {
+  # Combo contains the pair of variables to group by
+  variable1 <- combo[1]
+  variable2 <- combo[2]
+  
+  salarios_arg_cat2 <- base %>%
+    filter(!is.na(.data[[variable1]]) , !is.na(.data[[variable2]]),
+           .data[[variable1]] != "Ns/Nc",.data[[variable2]] != "Ns/Nc") %>% 
+    filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS == "Argentina") %>% 
+    group_by(PAIS,ANO,PERIODO,categoria1 = .data[[variable1]], categoria2 = .data[[variable2]]) %>% 
+    summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT_W,na.rm = T),
+              sal_median = median(ING_PPA)) 
+  
+  salarios_resto_cat2 <- base %>% 
+    filter(!is.na(.data[[variable1]]) , !is.na(.data[[variable2]]),
+           .data[[variable1]] != "Ns/Nc",.data[[variable2]] != "Ns/Nc") %>% 
+    filter(CATOCUP == "Asalariado",ING_PPA>0,PAIS != "Argentina") %>% 
+    group_by(PAIS,ANO,PERIODO,categoria1 = .data[[variable1]], categoria2 = .data[[variable2]]) %>% 
+    summarise(sal_prom =weighted.mean(ING_PPA,WEIGHT,na.rm = T),
+              sal_median = median(ING_PPA)) 
+  
+  salarios_cat2 <- bind_rows(salarios_arg_cat2,salarios_resto_cat2) %>% 
+    group_by(PAIS,ANO,categoria1,categoria2) %>% 
+    summarise(sal_prom = mean(sal_prom,na.rm = T),
+              sal_median = mean(sal_median,na.rm = T)) %>% 
+    mutate(variable_interes = paste(variable1, variable2, sep = "-")) 
+  
+  
+  salarios_categorias2 <- bind_rows(salarios_cat2,salarios_categorias2) 
+  
+  # Bind the results to the main data frame
+}
+
+salarios_categorias2<- salarios_categorias2 %>% 
+  mutate(categoria1 = factor(categoria1,levels = niveles),
+         categoria2 = factor(categoria2,levels = niveles)
+  ) %>% 
+  arrange(PAIS,categoria1,categoria2) %>% 
+  mutate(categoria = paste(categoria1, categoria2, sep = "-")) 
+
+write_csv(salarios_categorias2,"app/data/salarios_categoria2.csv")
+
+
+
